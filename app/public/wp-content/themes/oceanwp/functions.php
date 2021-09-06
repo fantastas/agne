acd119f82c49242aa4bd9fa20dad5ec3dbeea469
fantastas <msgitara@gmail.com>
2021-09-07 03:57:59 +0800

    Į krep

--- app/public/wp-content/themes/oceanwp/functions.php
+++ app/public/wp-content/themes/oceanwp/functions.php
@@ -1084,13 +1084,13 @@ if ( ! function_exists( 'owp_fs' ) ) {
 // To change add to cart text on single product page
 add_filter( 'woocommerce_product_single_add_to_cart_text', 'woocommerce_custom_single_add_to_cart_text' ); 
 function woocommerce_custom_single_add_to_cart_text() {
-    return __( 'Pirkti', 'woocommerce' ); 
+    return __( 'Į krepšelį', 'woocommerce' ); 
 }
 
 // To change add to cart text on product archives(Collection) page
 add_filter( 'woocommerce_product_add_to_cart_text', 'woocommerce_custom_product_add_to_cart_text' );  
 function woocommerce_custom_product_add_to_cart_text() {
-    return __( 'Į krepšelį', 'woocommerce' );
+    return __( 'Pridėti į krepšelį', 'woocommerce' );
 }
 
 

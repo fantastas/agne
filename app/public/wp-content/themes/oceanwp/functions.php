82bca8b97eccb74e9670d3c45e242ab9e6c324d6
fantastas <msgitara@gmail.com>
2021-09-07 02:47:54 +0800

    all

--- app/public/wp-content/themes/oceanwp/functions.php
+++ app/public/wp-content/themes/oceanwp/functions.php
@@ -1090,7 +1090,7 @@ function woocommerce_custom_single_add_to_cart_text() {
 // To change add to cart text on product archives(Collection) page
 add_filter( 'woocommerce_product_add_to_cart_text', 'woocommerce_custom_product_add_to_cart_text' );  
 function woocommerce_custom_product_add_to_cart_text() {
-    return __( 'Pirkti', 'woocommerce' );
+    return __( 'Į krepšelį', 'woocommerce' );
 }
 
 

a07745ee3925496b3202274bb2eb8a2862ff0e82
fantastas <msgitara@gmail.com>
2021-09-11 22:24:15 +0800

    istrinu nereikalingus pluginus

--- app/public/wp-content/plugins/qi-addons-for-elementor/inc/admin/inc/admin-pages/sub-pages/welcome/templates/welcome.php
+++
@@ -1,5 +0,0 @@
-<?php
-qi_addons_for_elementor_framework_template_part( QI_ADDONS_FOR_ELEMENTOR_ADMIN_PATH . '/inc', 'admin-pages', 'templates/parts/knowledge-base' );
-qi_addons_for_elementor_framework_template_part( QI_ADDONS_FOR_ELEMENTOR_ADMIN_PATH . '/inc', 'admin-pages', 'templates/parts/video-tutorials' );
-qi_addons_for_elementor_framework_template_part( QI_ADDONS_FOR_ELEMENTOR_ADMIN_PATH . '/inc', 'admin-pages', 'templates/parts/subscribe' );
-qi_addons_for_elementor_framework_template_part( QI_ADDONS_FOR_ELEMENTOR_ADMIN_PATH . '/inc', 'admin-pages', 'templates/parts/free-demos' );
--- app/public/wp-content/plugins/qi-addons-for-elementor/inc/masonry/templates/sizer-gutter.php
+++
@@ -1,4 +0,0 @@
-<?php if ( 'masonry' === $params ) { ?>
-	<div class="qodef-qi-grid-masonry-sizer"></div>
-	<div class="qodef-qi-grid-masonry-gutter"></div>
-<?php } ?>
